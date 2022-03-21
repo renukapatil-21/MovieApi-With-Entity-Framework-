4f4f36f68de849310d1107864bd1e1d4993b062d
Renuka Patil <69632043+renukapatil-21@users.noreply.github.com>
2022-03-21 17:17:25 +0530

    Update Movie.cs

--- MovieApiWithEF/Models/Movie.cs
+++ MovieApiWithEF/Models/Movie.cs
@@ -15,6 +15,6 @@ namespace MovieApiWithEF.Models
         [Required]
         public string Genre { get; set; }
         public DateTime? ReleaseDate { get; set; }
-        public string? Diector { get; set; }
+        public string? Director { get; set; }
     }
 }
